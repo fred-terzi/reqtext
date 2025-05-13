a1aa66f8571d66a14ee0919adb7eae4fcacffe9f
Joseph Terzi <terzitech@gmail.com>
2025-05-13 09:05:43 -0400

    doc: update help command usage instructions for clarity

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,9 +10,13 @@ Commands:
     help, --help, -h         Show help
 
     add_item, -a             Add an item to the end of the project
+        Usage: reqtext add_item <item_name>
 
     add_after, -aa           Add an item after a specified outline number
         Usage: reqtext add_after <outline_number> <item_name>
+
+    delete, -d               Delete an item by outline number
+        Usage: reqtext delete <outline_number>
     `;
     console.log(helpText.trim());
 }
